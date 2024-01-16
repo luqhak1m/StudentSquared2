3df365e3ec475923552bd13f1e5e7855fe6a66ac
Luqman <1211101583@student.mmu.edu.my>
2024-01-16 23:34:23 +0800

    Initial Commit

---
+++ StudentSquared2/StudentSquared2App.swift
@@ -0,0 +1,17 @@
+//
+//  StudentSquared2App.swift
+//  StudentSquared2
+//
+//  Created by Luqman Hakim on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct StudentSquared2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
