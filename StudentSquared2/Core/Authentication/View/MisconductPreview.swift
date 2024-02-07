73e497463fc6347910b9b8827b47411c957335e4
Luqman <1211101583@student.mmu.edu.my>
2024-02-08 03:07:51 +0800

    Match misconduct with student id

---
+++ StudentSquared2/Core/Authentication/View/MisconductPreview.swift
@@ -0,0 +1,18 @@
+//
+//  MisconductPreview.swift
+//  StudentSquared2
+//
+//  Created by Luqman Hakim on 07/02/2024.
+//
+
+import SwiftUI
+
+struct MisconductPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MisconductPreview()
+}
